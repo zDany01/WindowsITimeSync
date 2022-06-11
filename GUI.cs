64c6d752693f6fef9ff7045254d28235759dc2dc
zDany01 <85403430+zDany01@users.noreply.github.com>
2022-06-11 18:54:25 +0200

    Refactored Code

--- GUI.cs
+++ GUI.cs
@@ -15,52 +15,12 @@ namespace WindowsITimeSync
 
         public bool isInstalled => File.Exists(executablePath);
 
-
-        private void Uninstall(bool useShellDelete)
+        public GUI()
         {
-            try
-            {
-                currentUserRun.DeleteValue("TimeSync");
-                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + APP_NAME + ".url");
-                if (useShellDelete)
-                {
-                    Process.Start(HiddenProgram("cmd.exe", "/c timeout 3 & rmdir \"" + applicationDirectory + "\" /s /q"));
-                    Close();
-                }
-                else Directory.Delete(applicationDirectory, true);
-            }
-            catch (Exception ex)
-            {
-#if DEBUG
-                Debug.WriteLine("Exception when trying to uninstall the app: " + ex.Message);
-#endif
-            }
-
-        }
-
-
-        private void appShortcutToDesktop(string linkName) //I didn't think it was that hard to create a shortcut, i don't like to rely on WSHELL
-        //https://stackoverflow.com/questions/234231/creating-application-shortcut-in-a-directory 
-        {
-            using (StreamWriter writer = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + linkName + ".url"))
-            {
-                writer.WriteLine("[InternetShortcut]");
-                writer.WriteLine("URL=file:///" + executablePath);
-                writer.WriteLine("IconIndex=0");
-                writer.WriteLine("IconFile=" + executablePath.Replace('\\', '/'));
-                writer.Flush();
-            }
-        }
-
-        private ProcessStartInfo HiddenProgram(string fileName, string args)
-        {
-            return new ProcessStartInfo
-            {
-                FileName = fileName,
-                Arguments = args,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
+            InitializeComponent();
+            SyncBtn.Click += SyncBtn_Click;
+            StartupCbx.CheckedChanged += StartupCbx_CheckedChanged;
+            this.Load += GUI_Load;
         }
 
         public void SyncTime()
@@ -86,19 +46,57 @@ namespace WindowsITimeSync
             this.Enabled = true;
         }
 
-        public GUI()
-        {
-            InitializeComponent();
-            SyncBtn.Click += SyncBtn_Click;
-            StartupCbx.CheckedChanged += StartupCbx_CheckedChanged;
-            this.Load += GUI_Load;
-        }
-
         private void GUI_Load(object sender, EventArgs e)
         {
             StartupCbx.Checked = this.isInstalled;
         }
 
+        private ProcessStartInfo HiddenProgram(string fileName, string args)
+        {
+            return new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = args,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+        }
+        private void Uninstall(bool useShellDelete)
+        {
+            try
+            {
+                currentUserRun.DeleteValue("TimeSync");
+                File.Delete($"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\\{APP_NAME}.url");
+                if (useShellDelete)
+                {
+                    Process.Start(HiddenProgram("cmd.exe", $"/c timeout 3 & rmdir \"{applicationDirectory}\" /s /q"));
+                    Close();
+                }
+                else Directory.Delete(applicationDirectory, true);
+            }
+#if DEBUG
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine("Exception when trying to uninstall the app: " + ex.Message);
+            }
+#else
+            catch { }
+#endif
+
+        }
+        private void appShortcutToDesktop(string linkName) //I didn't think it was that hard to create a shortcut, i don't like to rely on WSHELL
+        //https://stackoverflow.com/questions/234231/creating-application-shortcut-in-a-directory 
+        {
+            using (StreamWriter writer = new StreamWriter($"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\\{linkName}.url"))
+            {
+                writer.WriteLine("[InternetShortcut]");
+                writer.WriteLine("URL=file:///" + executablePath);
+                writer.WriteLine("IconIndex=0");
+                writer.WriteLine("IconFile=" + executablePath.Replace('\\', '/'));
+                writer.Flush();
+            }
+        }
         private void StartupCbx_CheckedChanged(object sender, EventArgs e)
         {
             if (Application.ExecutablePath != executablePath)
@@ -120,7 +118,6 @@ namespace WindowsITimeSync
             }
 
         }
-
         private void SyncBtn_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("Your current system time will be synchronized using your local timezone settings\nDo you want to continue?", APP_NAME, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) SyncTime();
